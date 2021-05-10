0936c7bf111c42af29871d02f6728ae5aaad27e1
TroySandy <troysandy16@gmail.com>
2021-05-09 21:08:37 -0400

    page buttons working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import React from "react";
 import "./App.css";
-import Clock from "./components/Clock/Clock";
-import FetchChallenge from './components/TuesdayChallenge/TS_React_Challenge'
-import Extra from './components/V2_TuesChallenge/AdditionalWay'
-import NytApiSearch from './components/WednesdayChallenge/NytSearch'
-let testProp: string = "Am I getting passed to the Clock component?";
-let optionalProp: string = "You sure are!";
+import NytApiSearch from './components/NytApi/NytSearch'
 
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
       <div className="verticalCenter">
-        <Clock testProp={testProp} optionalProp={optionalProp} />
-        <hr/>
-        <FetchChallenge />
-        <hr/>
-        <Extra label={'Here is another way that I found'}/>
-        <hr/>
         <NytApiSearch />
       </div>
     </div>
--- src/components/NytApi/NytSearch.tsx
+++ src/components/NytApi/NytSearch.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './Nyt.css'
+import "./Nyt.css";
 
 type NytState = {
   searchTerm: string;
@@ -7,6 +7,7 @@ type NytState = {
   endDate: string;
   pageNumber: number;
   results: [];
+  button: boolean;
 };
 
 export default class NytApiSearch extends React.Component<{}, NytState> {
@@ -18,12 +19,15 @@ export default class NytApiSearch extends React.Component<{}, NytState> {
       endDate: "",
       pageNumber: 0,
       results: [],
+      button: true,
     };
   }
 
   FetchResults() {
     const baseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    const apiKey = "vr8WVzJQYplm06pmA6HRrEQ6uJmJlAtz";
+    const apiKey = "PTzMEc1pFhbOosnyP53bVlGGBJ0Jlaxi";
+
+    let pageNumber = this.state.pageNumber === 0 ? 0 : this.state.pageNumber;
 
     let url = `${baseURL}?api-key=${apiKey}&page=${this.state.pageNumber}&q=${this.state.searchTerm}`;
     url = this.state.startDate
@@ -34,10 +38,12 @@ export default class NytApiSearch extends React.Component<{}, NytState> {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
+        console.log(url);
+
         this.setState({
           results: data.response.docs,
         });
-        console.log(data.response.docs);
+        // console.log(data.response.docs);
       });
   }
 
@@ -47,35 +53,40 @@ export default class NytApiSearch extends React.Component<{}, NytState> {
     });
   };
 
-    changePageNumber = (
-      e: React.ChangeEventHandler<HTMLInputElement>,
-      direction: string
-    ) => {
-      if (direction === "down") {
-        if (this.state.pageNumber > 0) {
-          this.setState({
-            pageNumber: this.state.pageNumber - 1,
-          });
-          this.FetchResults();
-        }
-      }
-      if (direction === "up") {
-        this.setState({
-          pageNumber: this.state.pageNumber + 1,
-        });
-        this.FetchResults();
-      }
-    };
+  pageDown = () => {
+    console.log("down");
+
+    if (this.state.pageNumber > 0) {
+      this.setState({
+        pageNumber: this.state.pageNumber - 1,
+      });
+      console.log(this.state.pageNumber, "pageNumber");
+      this.FetchResults();
+    }
+  };
+
+  pageUp = () => {
+    console.log("up");
+
+    this.setState({
+      pageNumber: this.state.pageNumber + 1,
+    });
+    console.log(this.state.pageNumber, "pageNumber");
+
+    this.FetchResults();
+  };
 
   render() {
     return (
       <>
         <div>
           <form
+            className="form"
             onSubmit={(e: React.SyntheticEvent) => {
               e.preventDefault();
               this.setState({
                 pageNumber: 0,
+                button: false,
               });
               this.FetchResults();
             }}
@@ -122,13 +133,26 @@ export default class NytApiSearch extends React.Component<{}, NytState> {
             </button>
           </form>
           <hr />
+
+          <div hidden={this.state.button}>
+            <button
+              className="pageDown"
+              onClick={this.pageDown}
+            >
+              Previous Page
+            </button>
+            <button className="pageUp" onClick={this.pageUp}>
+              Next Page
+            </button>
+          </div>
+
           {this.state.results.map((result: any) => {
-            console.log("result", result);
+            // console.log("result", result);
             return (
               <div>
                 <NytDisplay
                   heading={result.abstract}
-                  img={result.multimedia[14].url}
+                  img={result.multimedia[1].url}
                   keys={result.keywords}
                   headline={result.headline.main}
                   web_url={result.web_url}
@@ -151,20 +175,34 @@ export interface IProps {
 }
 
 const NytDisplay = (props: IProps) => {
-  console.log("PROPS", props.keys);
+  // console.log("PROPS", props.keys);
   return (
-    <div>
-      <h2>{props.headline}</h2>
-      <h4>{props.heading}</h4>
-      <a href={props.web_url} target='_blank'>
-        <img src={`https://www.nytimes.com/${props.img}`} alt="nyt pic" />
+    <div className="results">
+      <h2 className="heading">{props.headline}</h2>
+      <h4 className="abstract">{props.heading}</h4>
+      <a href={props.web_url} target="_blank">
+        <img
+          src={`https://www.nytimes.com/${props.img}`}
+          alt="nyt pic"
+          className="image"
+        />
       </a>
-      {props.keys.map((key: any) => {
-        console.log(key);
-        //   console.log(key.value);
-        return <p>{key.value}</p>;
-      })}
-      <hr/>
+      <ul className="list">
+        {props.keys.map((key: any) => {
+          // console.log(key);
+          // console.log(key.value);
+          return <KeyWordDisplay keyword={key.value} />;
+        })}
+      </ul>
+      <hr />
     </div>
   );
+};
+
+export interface IKeywords {
+  keyword: string;
+}
+
+const KeyWordDisplay = (props: IKeywords) => {
+  return <li> {props.keyword} </li>;
 };
